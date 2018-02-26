2f2b66d983df74ef0710264c3909619b850faba7
AfukTju <andi.mlydi@gmail.com>
2018-02-26 16:29:17 +0700

    update

--- app/src/main/java/com/afuktju/oona/Utils.java
+++ app/src/main/java/com/afuktju/oona/Utils.java
@@ -1,6 +1,7 @@
 package com.afuktju.oona;
 
 import android.content.Context;
+import android.util.Log;
 
 /**
  * Created by AfukTju on 26/02/2018.
@@ -10,5 +11,9 @@ public class Utils {
     public static String getVideoPathByRaw(Context context, int video_file) {
         String path = "android.resource://" + context.getPackageName() + "/" + video_file;
         return path;
+    }
+
+    public static void showLog(String message) {
+        Log.d("LOGONA",message);
     }
 }
--- app/src/main/java/com/afuktju/oona/ui/main/MainActivity.java
+++ app/src/main/java/com/afuktju/oona/ui/main/MainActivity.java
@@ -5,79 +5,82 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.VideoView;
 
 import com.afuktju.oona.R;
 import com.afuktju.oona.Utils;
-import com.afuktju.oona.ui.playlist.TabVideoActivity;
+import com.afuktju.oona.OnSwipeTouchListener;
 import com.afuktju.oona.ui.advertisement.AdvertisementActivity;
+import com.afuktju.oona.ui.playlist.TabVideoActivity;
 
 /**
  * Created by AfukTju on 26/02/2018.
  */
 
-public class MainActivity extends FragmentActivity implements View.OnClickListener, MediaPlayer.OnCompletionListener {
-
-    public final static int DELAY_ADVERTISEMENT = 30 * 1000;
-    public final static int DELAY_LOGO = 10 * 1000;
-    Handler mHandler;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    VideoView vvMain;
     public int logos[] = {R.drawable.logo_facebook, 0, R.drawable.logo_linkedin, 0, R.drawable.logo_twitter, 0};
     public int videos[] = {R.raw.video_example_one, R.raw.video_example_two};
     private int counter_logo;
-    ImageView ivLogo;
-    private ViewPager viewpager;
-    private VideoPagerAdapter mPagerAdapter;
+    private ImageView ivLogo;
+    public final static int DELAY_LOGO = 10 * 1000;
+    public final static int DELAY_ADVERTISEMENT_IN_SECONDS = 30;
 
+    private int index_video = 0;
     public static final String KEY_ADVERTISEMENT_URL = "ADVERTISEMENT_URL";
-
-
+    private int progress = 0;
+    private Handler handlerProgressVideo;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_video);
-        mHandler = new Handler();
-        viewpager = findViewById(R.id.viewpager);
+        setContentView(R.layout.activity_single_video);
         ivLogo = findViewById(R.id.ivLogo);
         findViewById(R.id.ivPlayList).setOnClickListener(this);
         initializedLogo();
-        initializeVideo();
 
     }
 
-    public void initializeVideo() {
-        viewpager = findViewById(R.id.viewpager);
-        mPagerAdapter = new VideoPagerAdapter(getSupportFragmentManager());
-        mPagerAdapter.setup(this, videos);
-        mPagerAdapter.setOnCompletionListener(this);
-        viewpager.setAdapter(mPagerAdapter);
-        viewpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+    private void initializeMainVideo() {
+        vvMain = findViewById(R.id.vvMain);
+        vvMain.setVideoPath(Utils.getVideoPathByRaw(this, videos[index_video]));
+        OnSwipeTouchListener onSwipeTouchListener = new OnSwipeTouchListener(this) {
             @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
+            public void onSwipeRight() {
+                onChangeVideo();
             }
 
             @Override
-            public void onPageSelected(int position) {
-                showAdvertisement();
+            public void onSwipeLeft() {
+                onChangeVideo();
             }
-
+        };
+        vvMain.setOnTouchListener(onSwipeTouchListener);
+        vvMain.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
-            public void onPageScrollStateChanged(int state) {
+            public void onPrepared(MediaPlayer mediaPlayer) {
 
             }
         });
-        showAdvertisement();
-
+        vvMain.seekTo(progress);
+        vvMain.start();
     }
 
 
-    public void showAdvertisement() {
-        mHandler.removeCallbacks(mRunAdvertisement);
-        mHandler.postDelayed(mRunAdvertisement, DELAY_ADVERTISEMENT);
+    private void onChangeVideo() {
+        index_video = (index_video == 0 ? 1 : 0);
+        vvMain.setVideoPath(Utils.getVideoPathByRaw(this, videos[index_video]));
+        vvMain.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                onChangeVideo();
+            }
+        });
+        vvMain.start();
+
     }
 
     public void initializedLogo() {
@@ -94,17 +97,19 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
         }, DELAY_LOGO);
     }
 
-    Runnable mRunAdvertisement = new Runnable() {
-        @Override
-        public void run() {
-            Intent openAdvertisement = new Intent(MainActivity.this, AdvertisementActivity.class);
-            openAdvertisement.putExtra(KEY_ADVERTISEMENT_URL, "" + Utils.getVideoPathByRaw(MainActivity.this, R.raw.video_ten_second));
-            startActivity(openAdvertisement);
-            overridePendingTransition(0, 0);
+    private void initializeHandlerAdvertisement() {
+        if (handlerProgressVideo == null)
+            handlerProgressVideo = new Handler();
+        handlerProgressVideo.postDelayed(mRunUpdateProgressVideo, 1000);
+    }
 
-        }
-    };
 
+    public void showAdvertisement() {
+        Intent openAdvertisement = new Intent(this, AdvertisementActivity.class);
+        openAdvertisement.putExtra(KEY_ADVERTISEMENT_URL, "" + Utils.getVideoPathByRaw(this, R.raw.video_ten_second));
+        startActivity(openAdvertisement);
+        overridePendingTransition(0, 0);
+    }
 
     @Override
     public void onClick(View view) {
@@ -116,15 +121,34 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
     }
 
     @Override
-    public void onCompletion(MediaPlayer mediaPlayer) {
-        viewpager.setCurrentItem(viewpager.getCurrentItem() == 0 ? 1 : 0);
+    protected void onPause() {
+        super.onPause();
+        progress = vvMain.getCurrentPosition();
+        if (handlerProgressVideo != null)
+            handlerProgressVideo.removeCallbacks(mRunUpdateProgressVideo);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mPagerAdapter.notifyDataSetChanged();
+        initializeMainVideo();
+        initializeHandlerAdvertisement();
     }
 
 
+    Runnable mRunUpdateProgressVideo = new Runnable() {
+        @Override
+        public void run() {
+            double progressInSecond = Math.ceil(vvMain.getCurrentPosition() / 1000);
+            if (progressInSecond == (DELAY_ADVERTISEMENT_IN_SECONDS - 1)) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        showAdvertisement();
+                    }
+                }, 1000);
+            }
+            initializeHandlerAdvertisement();
+        }
+    };
 }
